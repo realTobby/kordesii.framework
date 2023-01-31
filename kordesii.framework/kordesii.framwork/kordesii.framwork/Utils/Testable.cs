82cd4a0d43b5f67e134b99e16c2f006c59c0c855
realTobby <tkattanek00@gmail.com>
2023-01-31 13:51:17 +0100

    Init
    I will forget about this anyways

---
+++ kordesii.framework/kordesii.framwork/kordesii.framwork/Utils/Testable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kordesii.framwork.Utils
+{
+    public static class Testable
+    {
+        public static bool GetRandomBool(Random seed)
+        {
+            return seed.Next(2) == 1;
+        }
+
+    }
+}
